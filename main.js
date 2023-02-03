a46ad68b7c353f69ab91f16afe082228b4cd1789
eunbinnie <dmsqlsdl54@naver.com>
2023-02-03 19:04:35 +0900

    clock, background image

--- main.js
+++ main.js
@@ -25,9 +25,27 @@ document.body.append(imgTag);
 
 
 // clock
-const date = document.querySelector(".date");
+const dateElement = document.querySelector(".date");
 const clock = document.querySelector(".clock");
-console.dir(clock);
-console.dir(date);
 
-const currentDate = new Date();
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function printClock() {
+  const currentDate = new Date();
+
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+  const day = days[currentDate.getDay()];
+  const month = months[currentDate.getMonth()];
+  const date = String(currentDate.getDate()).padStart(2, "0");
+  const year = currentDate.getFullYear();
+
+  dateElement.innerText = `${day} ${month} ${date} ${year}`;
+  clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+}
+
+printClock();
+setInterval(printClock, 1000);
