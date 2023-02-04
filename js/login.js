7e1a1338c5c820c1fa1b6a89e0daa03012504330
eunbinnie <dmsqlsdl54@naver.com>
2023-02-04 14:00:57 +0900

    login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 // local storage - LogIn
 const form = document.querySelector("form");
 const name = document.querySelector("input");
+const loginbutton = document.querySelector("#login-btn");
 
 function printName(username) {
   const welcomname = document.createElement('p');
@@ -12,6 +13,7 @@ function submitEvent() {
   localStorage.setItem('username', `${name.value}`);
   const username = localStorage.getItem('username');
   name.classList.add('hidden');
+  loginbutton.classList.add('hidden');
   printName(username);
 }
 
@@ -22,6 +24,7 @@ function loadEvent() {
     printName(username);
   } else {
     name.classList.remove('hidden');
+    loginbutton.classList.remove('hidden');
     form.addEventListener('submit', submitEvent);
   }
 }
