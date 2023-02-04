f81b61be6ad6538428db1810a45c43d8b3ef2e37
eunbinnie <dmsqlsdl54@naver.com>
2023-02-04 17:15:21 +0900

    todo 출력까지

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,1 +1,24 @@
 // TO DO list
+
+const listForm = document.querySelector(".list-form");
+const listInput = document.querySelector(".list-form input");
+const ul = document.querySelector("ul");
+
+const todos = [];
+
+function printToDo(todo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = `${todo}`;
+  li.appendChild(span);
+  ul.appendChild(li);
+}
+
+function enterToDoEvent() {
+  const value = listInput.value;
+  todos.push(value);
+  listInput.value = "";
+  printToDo(value);
+}
+
+listForm.addEventListener('submit', enterToDoEvent);
