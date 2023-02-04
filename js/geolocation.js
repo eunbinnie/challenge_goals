aedd419d3a27ee0a2bc2631ce55f7f3315b6bbfc
eunbinnie <dmsqlsdl54@naver.com>
2023-02-04 22:28:52 +0900

    geolocation, weather

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,2 +1,23 @@
 // weather, geolocation
 
+const API_KEY = "3cc2c00bbd487be4f723a9d9b7846ddc";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Cant't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
