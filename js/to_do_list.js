d8eaa56d3f4f1f29d06ed5ee4b90a7bb73c526bf
eunbinnie <dmsqlsdl54@naver.com>
2023-02-04 18:29:49 +0900

    to do list 출력까지

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -4,19 +4,33 @@ const listForm = document.querySelector(".list-form");
 const listInput = document.querySelector(".list-form input");
 const ul = document.querySelector("ul");
 
-const todos = [];
+const toDos = [];
 
+// 투두리스트 삭제
+function deleteClickEvent(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+// 투두리스트 출력
 function printToDo(todo) {
   const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerText = `${todo}`;
-  li.appendChild(span);
+  const list = document.createElement("span");
+  list.innerText = `${todo}`;
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "X";
+
+  deleteBtn.addEventListener('click', deleteClickEvent);
+
+  li.appendChild(list);
+  li.appendChild(deleteBtn);
   ul.appendChild(li);
 }
 
+// 투두리스트 입력
 function enterToDoEvent() {
   const value = listInput.value;
-  todos.push(value);
+  toDos.push(value);
   listInput.value = "";
   printToDo(value);
 }
