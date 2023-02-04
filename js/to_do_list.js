102473e7ce3ef7e292a6a35edcda12a5df02a59b
eunbinnie <dmsqlsdl54@naver.com>
2023-02-04 22:25:39 +0900

    to do list js 완성

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -4,17 +4,24 @@ const listForm = document.querySelector(".list-form");
 const listInput = document.querySelector(".list-form input");
 const ul = document.querySelector("ul");
 
-const toDos = [];
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem('todos', JSON.stringify(toDos));
+}
 
 // 투두리스트 삭제
 function deleteClickEvent(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 // 투두리스트 출력
 function printToDo(todo) {
   const li = document.createElement("li");
+  li.id = todo.id;
   const list = document.createElement("span");
   list.innerText = `${todo}`;
   const deleteBtn = document.createElement("button");
@@ -30,9 +37,24 @@ function printToDo(todo) {
 // 투두리스트 입력
 function enterToDoEvent() {
   const value = listInput.value;
-  toDos.push(value);
   listInput.value = "";
+  const newTodoObj = {
+    text: value,
+    id: Date.now(),
+  };
+  toDos.push(value);
   printToDo(value);
+  saveToDos();
 }
 
 listForm.addEventListener('submit', enterToDoEvent);
+
+const getTodos = localStorage.getItem('todos');
+
+if (getTodos !== null) {
+  const parseTodos = JSON.parse(getTodos);
+  toDos = parseTodos;
+  parseTodos.forEach(element => {
+    printToDo(element);
+  });
+}
