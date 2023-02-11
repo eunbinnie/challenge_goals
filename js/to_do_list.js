9bbb4acf281850e2d58ab8cd9e1aa0a03acadda0
eunbinnie <dmsqlsdl54@naver.com>
2023-02-11 14:55:38 +0900

    to do list error 수정해야함

--- js/to_do_list.js
+++ js/to_do_list.js
@@ -1,60 +1,52 @@
-// TO DO list
-
 const listForm = document.querySelector(".list-form");
-const listInput = document.querySelector(".list-form input");
-const ul = document.querySelector("ul");
+const todoInput = document.querySelector(".list-form input");
+const todoLists = document.querySelector("#to-do-list ul");
 
-let toDos = [];
+const todos = [];
 
-function saveToDos() {
-  localStorage.setItem('todos', JSON.stringify(toDos));
-}
-
-// 투두리스트 삭제
-function deleteClickEvent(event) {
+function deleteEvent(event) {
   const li = event.target.parentElement;
   li.remove();
-  toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
-  saveToDos();
+  todos = todos.filter(toDo => toDo.id !== parseInt(li.id));
+  console.log(todos)
+  inputEvent();
 }
 
-// 투두리스트 출력
-function printToDo(todo) {
-  const li = document.createElement("li");
-  li.id = todo.id;
-  const list = document.createElement("span");
-  list.innerText = `${todo}`;
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "X";
+function printList(item) {
+  const list = document.createElement('li');
+  list.id = item.id;
+  const span = document.createElement('span');
+  span.innerText = item;
+  list.appendChild(span);
 
-  deleteBtn.addEventListener('click', deleteClickEvent);
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete');
+  deleteBtn.innerText = "✖";
+  list.appendChild(deleteBtn);
+  deleteBtn.addEventListener('click', deleteEvent);
 
-  li.appendChild(list);
-  li.appendChild(deleteBtn);
-  ul.appendChild(li);
+  todoLists.appendChild(list);
 }
 
-// 투두리스트 입력
-function enterToDoEvent() {
-  const value = listInput.value;
-  listInput.value = "";
-  const newTodoObj = {
-    text: value,
-    id: Date.now(),
-  };
-  toDos.push(value);
-  printToDo(value);
-  saveToDos();
-}
-
-listForm.addEventListener('submit', enterToDoEvent);
-
-const getTodos = localStorage.getItem('todos');
-
-if (getTodos !== null) {
-  const parseTodos = JSON.parse(getTodos);
-  toDos = parseTodos;
-  parseTodos.forEach(element => {
-    printToDo(element);
+function getLocalStorage() {
+  const fromLocalItem = localStorage.getItem('todos');
+  const localList = JSON.parse(fromLocalItem);
+  console.log(localList);
+  localList.forEach(element => {
+    todos.push(element);
+    printList(element);
   });
 }
+
+function inputEvent() {
+  const inputValue = todoInput.value;
+
+  todos.push(inputValue);
+  localStorage.setItem('todos', JSON.stringify(todos));
+  printList(inputValue);
+
+  todoInput.value = "";
+}
+
+listForm.addEventListener('submit', inputEvent);
+window.addEventListener('load', getLocalStorage);
